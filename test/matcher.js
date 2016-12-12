65c033b7394c8881ddac193bbae05624f939b07b
robindanzinger <robindanzinger@gmx.de>
2016-12-12 17:32:45 +0100

    refactored

--- test/matcher.js
+++ test/matcher.js
@@ -1,27 +1,60 @@
 'use strict';
 
 var expect = require('must');
-var matcher = require('../lib/matcher');
+var matchers = require('../lib/matcher');
+
+function shouldMatch(matcher, values) {
+  values.forEach(function(value) {
+    try {
+      expect(matcher.matches(value)).to.be.true();
+    } catch (error) {
+      var message = 'should match ' + value + ' but didn\'t.';
+      throw new Error(message);
+    }
+  });
+}
+
+function shouldNotMatch(matcher, values) {
+  values.forEach(function(value) {
+    try {
+      expect(matcher.matches(value)).to.be.false();
+    } catch (error) {
+      var message = 'shouldn\'t match ' + value + ' but did.';
+      throw new Error(message);
+    }
+  });
+}
 
 describe('Library "matcher"', function () {
   it('anyValue matches any value', function () {
-    expect(matcher.anyValue.matches()).to.be.true();
-    expect(matcher.anyValue.matches(undefined)).to.be.true();
-    expect(matcher.anyValue.matches(null)).to.be.true();
-    expect(matcher.anyValue.matches('a string')).to.be.true();
-    expect(matcher.anyValue.matches(1)).to.be.true();
-    expect(matcher.anyValue.matches(new Date())).to.be.true();
-    expect(matcher.anyValue.matches([1, 2, 3])).to.be.true();
-    expect(matcher.anyValue.matches({foo: 'bar', bar: 'foo'})).to.be.true();
+    shouldMatch(matchers.anyValue, [
+      undefined,
+      null,
+      'a string',
+      1,
+      new Date(),
+      [],
+      [1, 2, 3],
+      {foo: 'bar', bar: 'foo'}
+    ]);
+    expect(matchers.anyValue.matches()).to.be.true();
   });
+
   it('notNull matches any value but null and undefined', function () {
-    expect(matcher.notNull.matches()).to.be.false();
-    expect(matcher.notNull.matches(undefined)).to.be.false();
-    expect(matcher.notNull.matches(null)).to.be.false();
-    expect(matcher.notNull.matches('a string')).to.be.true();
-    expect(matcher.notNull.matches(1)).to.be.true();
-    expect(matcher.notNull.matches(new Date())).to.be.true();
-    expect(matcher.notNull.matches([1, 2, 3])).to.be.true();
-    expect(matcher.notNull.matches({foo: 'bar', bar: 'foo'})).to.be.true();
+   shouldMatch(matchers.notNull, [
+      'a string',
+      1,
+      new Date(),
+      [],
+      [1, 2, 3],
+      {foo: 'bar', bar: 'foo'}
+    ]);
+    shouldNotMatch(matchers.notNull, [
+      undefined,
+      null
+    ]);
+    expect(matchers.notNull.matches()).to.be.false();
+  });
+  it.skip('anyNumber matches any Number', function () {
   });
 });
