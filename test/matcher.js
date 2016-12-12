1e82f76d16dc46386e0e224402bed0893230b5ec
robindanzinger <robindanzinger@gmx.de>
2016-12-12 17:55:07 +0100

    new matcher anyNumber

--- test/matcher.js
+++ test/matcher.js
@@ -4,25 +4,43 @@ var expect = require('must');
 var matchers = require('../lib/matcher');
 
 function shouldMatch(matcher, values) {
+  var errors = [];
   values.forEach(function(value) {
     try {
       expect(matcher.matches(value)).to.be.true();
     } catch (error) {
-      var message = 'should match ' + value + ' but didn\'t.';
-      throw new Error(message);
+      if (error.name === 'AssertionError') {
+        var message = 'should match ' + value + ' but didn\'t.';
+        errors.push(message);
+      } else {
+        throw error;
+      }
     }
   });
+  handleOptionalErrors(errors);
 }
 
 function shouldNotMatch(matcher, values) {
+  var errors = [];
   values.forEach(function(value) {
     try {
       expect(matcher.matches(value)).to.be.false();
     } catch (error) {
-      var message = 'shouldn\'t match ' + value + ' but did.';
-      throw new Error(message);
+      if (error.name === 'AssertionError') {
+        var message = 'shouldn\'t match ' + value + ' but did.';
+        errors.push(message);
+      } else {
+        throw error;
+      }
     }
   });
+  handleOptionalErrors(errors);
+}
+
+function handleOptionalErrors(errors) {
+  if (errors.length > 0) {
+    throw new Error(errors.join(',\n'));
+  }
 }
 
 describe('Library "matcher"', function () {
@@ -35,6 +53,7 @@ describe('Library "matcher"', function () {
       new Date(),
       [],
       [1, 2, 3],
+      {},
       {foo: 'bar', bar: 'foo'}
     ]);
     expect(matchers.anyValue.matches()).to.be.true();
@@ -47,6 +66,7 @@ describe('Library "matcher"', function () {
       new Date(),
       [],
       [1, 2, 3],
+      {},
       {foo: 'bar', bar: 'foo'}
     ]);
     shouldNotMatch(matchers.notNull, [
@@ -55,6 +75,29 @@ describe('Library "matcher"', function () {
     ]);
     expect(matchers.notNull.matches()).to.be.false();
   });
-  it.skip('anyNumber matches any Number', function () {
+  it('anyNumber matches any Number', function () {
+    shouldMatch(matchers.anyNumber, [
+      0,
+      -1,
+      1,
+      Number.EPSILON,
+      Number.MAX_SAFE_INTEGER,
+      Number.MAX_VALUE,
+      Number.MIN_SAFE_INTEGER,
+      Number.MIN_VALUE,
+      Number.NEGATIVE_INFINITY,
+      Number.POSITIVE_INFINITY
+    ]);
+
+    shouldNotMatch(matchers.anyNumber, [
+      undefined,
+      null,
+      'a string',
+      [],
+      [1, 2, 3],
+      {},
+      {foo: 'bar'},
+      Number.NaN
+    ]);
   });
 });
