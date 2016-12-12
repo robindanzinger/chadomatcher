f5aeb582cecd6859b7061d3598615567913e726f
robindanzinger <robindanzinger@gmx.de>
2016-12-12 17:09:02 +0100

    added matcher for anyValue and notNull

--- test/matcher.js
+++ test/matcher.js
@@ -4,7 +4,24 @@ var expect = require('must');
 var matcher = require('../lib/matcher');
 
 describe('Library "matcher"', function () {
-  it('matches identical objects', function () {
-    expect(matcher.match()).to.be.true();
+  it('anyValue matches any value', function () {
+    expect(matcher.anyValue.matches()).to.be.true();
+    expect(matcher.anyValue.matches(undefined)).to.be.true();
+    expect(matcher.anyValue.matches(null)).to.be.true();
+    expect(matcher.anyValue.matches('a string')).to.be.true();
+    expect(matcher.anyValue.matches(1)).to.be.true();
+    expect(matcher.anyValue.matches(new Date())).to.be.true();
+    expect(matcher.anyValue.matches([1, 2, 3])).to.be.true();
+    expect(matcher.anyValue.matches({foo: 'bar', bar: 'foo'})).to.be.true();
+  });
+  it('notNull matches any value but null and undefined', function () {
+    expect(matcher.notNull.matches()).to.be.false();
+    expect(matcher.notNull.matches(undefined)).to.be.false();
+    expect(matcher.notNull.matches(null)).to.be.false();
+    expect(matcher.notNull.matches('a string')).to.be.true();
+    expect(matcher.notNull.matches(1)).to.be.true();
+    expect(matcher.notNull.matches(new Date())).to.be.true();
+    expect(matcher.notNull.matches([1, 2, 3])).to.be.true();
+    expect(matcher.notNull.matches({foo: 'bar', bar: 'foo'})).to.be.true();
   });
 });
